e4e162ecd35812aa024caa3264cdff6fc3255742
Thomas Ngo <tngo0508@csu.fullerton.edu>
2016-11-29 14:07:56 -0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,24 +1,46 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 class Graph {
 public:
 	// TO DO
 	// initialize an undirected graph that can store at most n vertices
 	Graph(const int n) {
-
+		v = n;
+		adj = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			adj[i] = new int[n];
+			for (int j = 0; j < n; j++)
+			{
+				adj[i][j] = 0;
+			}
+		}
 	}
 
 	// TO DO
 	// insert an edge between vertices u and v
 	void insertEdge(int u, int v) {
-
+		if (u > this->v || v > this->v || u < 0 || v < 0)
+		{
+			throw invalid_argument("invalid vertices");
+		}
+		else
+			adj[u - 1][v - 1] = 1;
 	}
 
 	// TO DO
 	// return the set of vertices that are adjacent to v
 	std::vector<int> adjacentVertices(int v) {
 		std::vector<int> vertices;
+		for (int i = 0; i < v; i++)
+		{
+			if (adj[i][v] == 1)
+			{
+				vertices.push_back(i);
+			}
+		}
 
 		return vertices;
 	}
@@ -28,12 +50,14 @@ public:
 	// vertices should not be repeated
 	std::vector<int> getPath(int v, int w) {
 		std::vector<int> path;
-
+		
 		return path;
 	}
 
 private:
 	// TO DO
 	// member variables and functions to implement the public member functions
-
+	int v; //number of vertices
+	int **adj;
 };
+
